16eec5224917a0557a03db8a6defa5055a5e5c38
gangesm <sopan@cart91.com>
2016-04-30 13:47:55 +0530

    addition function

---
+++ js-assignment1/function.js
@@ -0,0 +1,31 @@
+
+------------ addition formula ---------------
+
+function addition(number1, number2, number3){
+  var result;
+  console.log("number1", number1);
+  console.log("number2", number2);
+  console.log("number3", number3);
+  console.log("result before addition is", result);
+  result = number1 + number2;
+  console.log("result after addition is", result);
+  return result;
+}
+
+-------------------- Area of rectangle ----------------
+
+function areaOfreatangle(length, weight) {
+  var result;
+  console.log("length", length);
+  console.log("weight", weight);
+  result = length * weight;
+  console.log("areaOfreatangle", result);
+}
+
+
+
+------------------ find subject percentage using array -------------
+
+function percentageOfsubject(argument) {
+  // body...
+}
